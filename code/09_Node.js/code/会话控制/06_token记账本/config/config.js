8782f7280b43adfdee4f531cf2a83821c4fb6537
zhangqicheng <2331256903@qq.com>
2023-09-04 09:37:57 +0800

    删除重复文件

--- code/09_Node.js/code/会话控制/06_token记账本/config/config.js
+++
@@ -1,7 +0,0 @@
-// 配置文件
-module.exports = {
-  DBHOST: '127.0.0.1',
-  DBPORT: '27017',
-  DBNAME: 'bilibili',
-  secret: 'atguigu'
-}
--- code/09_Node.js/code/会话控制/06_token记账本/middlewares/checkLoginMiddleware.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res, next) => {
-  // 判断
-  if (!req.session.username) {
-    return res.redirect('/login')
-  }
-  next()
-}
